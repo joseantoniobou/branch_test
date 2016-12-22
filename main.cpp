fa3aad3bdb4b454cc5931981e78252d675e52bf9
joseantoniobou <joseantoniobou@icloud.com>
2016-12-22 06:14:53 +0100

    Imprimo en pantalla una tonteria

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 	cout << "Hola mundo branch master" << endl;
 	
 	cout << "Agrego esta linea en el branch." << endl;
+	cout << "Mas modificaciones del branch." << endl;	
 
 	return 0;	
 }
